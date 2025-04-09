90314a3c3dd5125eedfc2bc3d678dcf647d59add
MauricioJrB <mjunior1205@gmail.com>
2025-04-08 21:30:44 -0300

    refactor: changed server start

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,6 @@
 import { app } from './app';
+import { config } from './config';
 
-const PORT = process.env.PORT || 3333;
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(config.PORT, () => {
+  console.log(`Server running on http://localhost:${config.PORT}`);
 });

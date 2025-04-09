bfcc989c2df2bef99e7df85baff00cd01a3733ca
MauricioJrB <mjunior1205@gmail.com>
2025-04-09 17:23:10 -0300

    feat: added AuthenticateUserDTO

--- backend/src/modules/users/routes/userRoutes.ts
+++ backend/src/modules/users/routes/userRoutes.ts
@@ -1,11 +1,17 @@
+import { AuthenticateUserController } from '../controllers/AuthenticateUserController';
 import { RegisterUserController } from '../controllers/RegisterUserController';
 import { Request, Response, Router } from 'express';
 
 const userRoutes = Router();
 const registerUserController = new RegisterUserController();
+const authenticateUserController = new AuthenticateUserController();
 
 userRoutes.post('/register', (request: Request, response: Response) => {
   registerUserController.handle(request, response);
+});
+
+userRoutes.post('/login', (request: Request, response: Response) => {
+  authenticateUserController.handle(request, response);
 });
 
 export { userRoutes };

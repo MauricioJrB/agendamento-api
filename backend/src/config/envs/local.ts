10dd4b26341588e26a64a1f05de01c39466af340
MauricioJrB <mjunior1205@gmail.com>
2025-04-09 17:22:10 -0300

    added jwt secret environment variable

--- backend/src/config/envs/local.ts
+++ backend/src/config/envs/local.ts
@@ -5,6 +5,6 @@ export function createLocalConfig() {
     PORT: Number(process.env.PORT || 3000),
     HOST: process.env.HOST || '0.0.0.0',
     SALT_ROUNDS: Number(process.env.SALT_ROUNDS),
-    //JWT_SECRET: process.env.JWT_SECRET,
+    JWT_SECRET: process.env.JWT_SECRET as string,
   });
 }
--- backend/src/config/types.ts
+++ backend/src/config/types.ts
@@ -4,7 +4,7 @@ export const appConfigSchema = z.object({
   PORT: z.number(),
   HOST: z.string(),
   SALT_ROUNDS: z.number(),
-  //JWT_SECRET: z.string(),
+  JWT_SECRET: z.string(),
 });
 
 export type AppConfig = z.infer<typeof appConfigSchema>;

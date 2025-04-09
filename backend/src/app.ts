4caf629cc7af79aba7979b134fb5c2ca927887c8
MauricioJrB <mjunior1205@gmail.com>
2025-04-09 17:26:30 -0300

    feat: renamed userRoutes file in app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { userRoutes } from './modules/users/routes/routes';
+import { userRoutes } from './modules/users/routes/userRoutes';
 
 const app = express();
 

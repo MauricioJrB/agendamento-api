ce1c22efd244ca40a21e4158c03ee13ecccc91ea
MauricioJrB <mjunior1205@gmail.com>
2025-04-10 18:07:00 -0300

    feat: added appointmentsRoutes and serviceRoutes in app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import { userRoutes } from './modules/users/routes/userRoutes';
+import { appointmentRoutes } from './modules/appointments/routes/appointmentRoutes';
+import { serviceRoutes } from './modules/services/routes/servicesRoutes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use('/users', userRoutes);
+app.use('/appointments', appointmentRoutes);
+app.use('/services', serviceRoutes);
 
 export { app };

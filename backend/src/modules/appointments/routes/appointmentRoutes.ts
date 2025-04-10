4c47358fa5372697683de13a4a16c081c76be840
MauricioJrB <mjunior1205@gmail.com>
2025-04-10 18:02:16 -0300

    feat: added appointmentRoutes put to update

--- backend/src/modules/appointments/routes/appointmentRoutes.ts
+++ backend/src/modules/appointments/routes/appointmentRoutes.ts
@@ -1,16 +1,26 @@
 import { Request, Response, Router } from 'express';
 import { authenticateToken } from '../../../middlewares/authenticateToken';
 import { CreateAppointmentController } from '../controllers/CreateAppointmentController';
+import { CancelAppointmentController } from '../controllers/CancelAppointmentController';
 
 const appointmentRoutes = Router();
 
 const createAppointmentController = new CreateAppointmentController();
+const cancelAppointmentController = new CancelAppointmentController();
 
 appointmentRoutes.post(
   '/',
   authenticateToken,
   (request: Request, response: Response) => {
     createAppointmentController.handle(request, response);
+  }
+);
+
+appointmentRoutes.put(
+  '/cancel/:appointmentId',
+  authenticateToken,
+  (request: Request, response: Response) => {
+    cancelAppointmentController.handle(request, response);
   }
 );
 

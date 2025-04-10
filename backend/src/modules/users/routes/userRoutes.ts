df130eeaa52f85c6bc2bf34620890dfdb95152f2
MauricioJrB <mjunior1205@gmail.com>
2025-04-10 17:55:59 -0300

    feat: used authenticateToken in userRoutes to get profile test

--- backend/src/modules/users/routes/userRoutes.ts
+++ backend/src/modules/users/routes/userRoutes.ts
@@ -1,3 +1,4 @@
+import { authenticateToken } from '../../../middlewares/authenticateToken';
 import { AuthenticateUserController } from '../controllers/AuthenticateUserController';
 import { RegisterUserController } from '../controllers/RegisterUserController';
 import { Request, Response, Router } from 'express';
@@ -13,5 +14,13 @@ userRoutes.post('/register', (request: Request, response: Response) => {
 userRoutes.post('/login', (request: Request, response: Response) => {
   authenticateUserController.handle(request, response);
 });
+
+userRoutes.get(
+  '/profile',
+  authenticateToken,
+  (request: Request, response: Response) => {
+    response.status(200).json({ message: `Hello, user ${request.user.id}` });
+  }
+);
 
 export { userRoutes };

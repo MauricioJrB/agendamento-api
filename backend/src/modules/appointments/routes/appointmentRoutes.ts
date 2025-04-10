37fa43c4a4812edfed5a0c9f20d679dde9afdf51
MauricioJrB <mjunior1205@gmail.com>
2025-04-10 18:06:36 -0300

    feat: added appointmentRoutes get to list appointments

--- backend/src/modules/appointments/routes/appointmentRoutes.ts
+++ backend/src/modules/appointments/routes/appointmentRoutes.ts
@@ -2,11 +2,13 @@ import { Request, Response, Router } from 'express';
 import { authenticateToken } from '../../../middlewares/authenticateToken';
 import { CreateAppointmentController } from '../controllers/CreateAppointmentController';
 import { CancelAppointmentController } from '../controllers/CancelAppointmentController';
+import { ListUserAppointmentsController } from '../controllers/ListUserAppointmentsController';
 
 const appointmentRoutes = Router();
 
 const createAppointmentController = new CreateAppointmentController();
 const cancelAppointmentController = new CancelAppointmentController();
+const listUserAppointmentsController = new ListUserAppointmentsController();
 
 appointmentRoutes.post(
   '/',
@@ -21,6 +23,14 @@ appointmentRoutes.put(
   authenticateToken,
   (request: Request, response: Response) => {
     cancelAppointmentController.handle(request, response);
+  }
+);
+
+appointmentRoutes.get(
+  '/',
+  authenticateToken,
+  (request: Request, response: Response) => {
+    listUserAppointmentsController.handle(request, response);
   }
 );
 

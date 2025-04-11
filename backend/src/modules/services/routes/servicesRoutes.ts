027f5dd4543427231b037b9b8c0bf4459687252e
MauricioJrB <mjunior1205@gmail.com>
2025-04-11 16:16:59 -0300

    feat: added serviceRoutes put to update service

--- backend/src/modules/services/routes/servicesRoutes.ts
+++ backend/src/modules/services/routes/servicesRoutes.ts
@@ -1,9 +1,12 @@
 import { Request, Response, Router } from 'express';
 import { CreateServiceController } from '../controllers/CreateServiceController';
 import { authenticateToken } from '../../../middlewares/authenticateToken';
+import { UpdateServiceController } from '../controllers/UpdateServiceController';
 
 const serviceRoutes = Router();
+
 const createServiceController = new CreateServiceController();
+const updateServiceController = new UpdateServiceController();
 
 serviceRoutes.post(
   '/',
@@ -12,5 +15,9 @@ serviceRoutes.post(
     createServiceController.handle(request, response);
   }
 );
+
+serviceRoutes.put('/:serviceId', (request: Request, response: Response) => {
+  updateServiceController.handle(request, response);
+});
 
 export { serviceRoutes };

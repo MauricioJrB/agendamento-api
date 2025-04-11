f96390e7b89ddd2161165fb42d4f8e802306e801
MauricioJrB <mjunior1205@gmail.com>
2025-04-11 16:36:48 -0300

    feat: adde serviceRoutes get to list all

--- backend/src/modules/services/routes/servicesRoutes.ts
+++ backend/src/modules/services/routes/servicesRoutes.ts
@@ -2,11 +2,13 @@ import { Request, Response, Router } from 'express';
 import { CreateServiceController } from '../controllers/CreateServiceController';
 import { authenticateToken } from '../../../middlewares/authenticateToken';
 import { UpdateServiceController } from '../controllers/UpdateServiceController';
+import { ListServiceController } from '../controllers/ListServiceController';
 
 const serviceRoutes = Router();
 
 const createServiceController = new CreateServiceController();
 const updateServiceController = new UpdateServiceController();
+const listServiceController = new ListServiceController();
 
 serviceRoutes.post(
   '/',
@@ -18,6 +20,10 @@ serviceRoutes.post(
 
 serviceRoutes.put('/:serviceId', (request: Request, response: Response) => {
   updateServiceController.handle(request, response);
+});
+
+serviceRoutes.get('/', (request: Request, response: Response) => {
+  listServiceController.handle(request, response);
 });
 
 export { serviceRoutes };

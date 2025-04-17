40a9a14c95a04ea1e4567d8d5c57278a1ed3bdf2
MauricioJrB <mjunior1205@gmail.com>
2025-04-17 16:15:59 -0300

    refactor: updated CreateAppointmetController

--- backend/src/modules/appointments/controllers/CreateAppointmentController.ts
+++ backend/src/modules/appointments/controllers/CreateAppointmentController.ts
@@ -1,25 +1,33 @@
 import { ptBR } from 'date-fns/locale';
 import { parse } from 'date-fns';
+import { prisma } from '../../../utils/prisma';
 import { Request, Response } from 'express';
 import { CreateAppointmentService } from '../services/CreateAppointmentService';
+import { PrismaAppointmentRepository } from '../repositories/PrismaAppointmentRepository';
 
 export class CreateAppointmentController {
   public async handle(request: Request, response: Response) {
-    const { date, serviceId } = request.body;
-    const userId = request.user.id;
+    try {
+      const { date, serviceId } = request.body;
+      const userId = request.user.id;
 
-    const parsedDate = parse(date, 'dd/MM/yyyy HH:mm', new Date(), {
-      locale: ptBR,
-    });
+      const parsedDate = parse(date, 'dd/MM/yyyy HH:mm', new Date(), {
+        locale: ptBR,
+      });
 
-    const service = new CreateAppointmentService();
+      const repository = PrismaAppointmentRepository.build(prisma);
+      const service = CreateAppointmentService.build(repository);
 
-    const appointment = await service.execute({
-      date: parsedDate,
-      serviceId,
-      userId,
-    });
+      const appointment = await service.execute({
+        date: parsedDate,
+        serviceId,
+        userId,
+      });
 
-    return response.status(201).json(appointment);
+      return response.status(201).json(appointment);
+    } catch (error) {
+      if (error instanceof Error)
+        return response.status(400).json({ error: error.message });
+    }
   }
 }
